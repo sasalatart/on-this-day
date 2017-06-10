efa4c57e717535ffd21ce74a8bf75502d625d7b2
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-06-10 18:24:13 -0400

    refactor: remove useless boilerplate

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -8,13 +8,15 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
+/* global navigator, window */
+/* eslint-disable no-param-reassign, no-console */
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       navigator.serviceWorker
         .register(swUrl)
-        .then(registration => {
+        .then((registration) => {
           registration.onupdatefound = () => {
             const installingWorker = registration.installing;
             installingWorker.onstatechange = () => {
@@ -35,7 +37,7 @@ export default function register() {
             };
           };
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error during service worker registration:', error);
         });
     });
@@ -44,7 +46,7 @@ export default function register() {
 
 export function unregister() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
+    navigator.serviceWorker.ready.then((registration) => {
       registration.unregister();
     });
   }
