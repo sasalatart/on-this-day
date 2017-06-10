425122472f29a449ae2971e61c7c96279e8ac9b4
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-06-10 14:20:13 -0400

    feat: init server

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('short'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
