7c6d5c20cdbeabc9db2d0588dca0b101f274896a
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-01-05 00:50:37 -0300

    refactor: use async/await in episodes route

--- server/routes/episodes.js
+++ server/routes/episodes.js
@@ -3,19 +3,19 @@ const checkValidRequest = require('../middlewares/valid-request');
 const Day = require('../models/day');
 const { createSelector } = require('../models/day');
 
-router.get('/', checkValidRequest, (req, res, next) => {
+router.get('/', checkValidRequest, async (req, res, next) => {
   const { day: intDay, month: intMonth, type, short } = req.query;
 
-  Day
-    .findOne({ day: intDay, month: intMonth }, createSelector(type, short))
-    .then((day) => {
-      if (day) {
-        res.status(200).json(day);
-      } else {
-        next({ message: 'Day not found', statusCode: 404 });
-      }
-    })
-    .catch(next);
+  try {
+    const day = await Day.findOne({ day: intDay, month: intMonth }, createSelector(type, short));
+    if (day) {
+      res.status(200).json(day);
+    } else {
+      next({ message: 'Day not found', statusCode: 404 });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
