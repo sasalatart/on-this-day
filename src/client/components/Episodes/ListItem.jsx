10ca0b56dd464198f29aafca741eabca8756139e
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-01-07 19:47:31 -0300

    refactor: move utils/keywords functions into EpisodesListItem file

--- src/client/components/Episodes/ListItem.jsx
+++ src/client/components/Episodes/ListItem.jsx
@@ -4,11 +4,26 @@ import { ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Avatar from 'material-ui/Avatar';
 import { blue900, transparent } from 'material-ui/styles/colors';
-import generateKeywordsText from '../../utils/keywords';
 import customPropTypes from '../../custom-prop-types';
 
-const EpisodeListItem = (props) => {
-  const { _id, data, year, kw } = props;
+function generateKeywordsText(kw) {
+  if (!kw) {
+    return undefined;
+  }
+
+  const wikiUrl = 'http://wikipedia.org';
+
+  const keyWords = kw
+    .map(({ href, title }) => (
+      <a key={`${href}${title}`} href={`${wikiUrl}/${href}`} target="_blank" rel="noopener noreferrer">
+        {title}
+      </a>
+    ));
+
+  return <p className="keywords">Keywords: {keyWords}</p>;
+}
+
+const EpisodeListItem = ({ _id, data, year, kw }) => {
   const leftAvatar = (
     <Avatar color={blue900} backgroundColor={transparent}>
       <p className="centered-text">{year}</p>
