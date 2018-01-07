8795ca7b393009d07fb04c10e3e03005c8ae4b57
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-01-07 19:23:48 -0300

    refactor: move server dir inside src dir

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker';
-import Router from './router';
+import Router from './client/router';
 import '../node_modules/sweetalert/dist/sweetalert.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import './styles/index.css';
+import './client/styles/index.css';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
