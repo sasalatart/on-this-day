ab804d995a602937c1a57050ffb8a76446a4a7ec
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-06-07 22:11:43 -0400

    🔧 change docker mongodb host name from 'mongo_db' to 'mongo'

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bluebird = require('bluebird');
 
 const DB_URI = process.env.MONGODB_URI;
-const DB_HOST = process.env.DB_HOST || 'mongo_db';
+const DB_HOST = process.env.DB_HOST || 'mongo';
 const DB_PORT = process.env.DB_PORT || '27017';
 mongoose.connect(DB_URI || `mongodb://${DB_HOST}:${DB_PORT}/on-this-day`);
 mongoose.Promise = bluebird;
