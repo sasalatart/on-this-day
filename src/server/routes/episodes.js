8e0bed03eae0c25769997a89a462c7e271011736
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-06-14 21:03:28 -0400

    ♻️ simplify api route for fetching episodes

--- src/server/routes/episodes.js
+++ src/server/routes/episodes.js
@@ -1,25 +1,16 @@
 const router = require('express').Router();
 const checkValidRequest = require('../middlewares/valid-request');
 const Day = require('../models/day');
-const { createSelector } = require('../models/day');
 
 router.get('/', checkValidRequest, async (req, res, next) => {
-  const {
-    day: intDay,
-    month: intMonth,
-    type,
-    short,
-  } = req.query;
+  const { type, short, ...query } = req.query;
 
-  try {
-    const day = await Day.findOne({ day: intDay, month: intMonth }, createSelector(type, short));
-    if (day) {
-      res.status(200).json(day);
-    } else {
-      next({ message: 'Day not found', statusCode: 404 });
-    }
-  } catch (error) {
-    next(error);
+  const day = await Day.findOne(query, Day.createSelector(type, short));
+
+  if (day) {
+    res.status(200).json(day);
+  } else {
+    next({ message: 'Day not found', statusCode: 404 });
   }
 });
 
