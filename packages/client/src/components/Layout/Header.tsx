e55732c08718e15bc8b1188ffa712f62e727ad09
Sebastián Salata R-T <sa.salatart@gmail.com>
2020-04-16 23:15:55 -0400

    feat: add GitHub repo link to app-bar

--- packages/client/src/components/Layout/Header.tsx
+++ packages/client/src/components/Layout/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icons, REPO_URL } from '../../config';
 import { Link, StyledTheme } from '../common';
 
 const Title = styled(Typography)`
@@ -19,6 +21,15 @@ export default function Header(): JSX.Element {
         <Title variant="h6">
           <Link to="/">On This Day</Link>
         </Title>
+        <Button
+          color="inherit"
+          startIcon={<FontAwesomeIcon icon={icons.github} color="white" />}
+          href={REPO_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </Button>
       </Toolbar>
     </AppBar>
   );
