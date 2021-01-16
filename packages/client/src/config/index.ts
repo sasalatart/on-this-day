d22e26539d933b023f59a6fca7efba29617bbff1
Sebastián Salata R-T <sa.salatart@gmail.com>
2021-01-16 17:48:38 -0300

    fix: use origin instead of host for server url in prod

--- packages/client/src/config/index.ts
+++ packages/client/src/config/index.ts
@@ -2,7 +2,7 @@ export { default as theme } from './theme';
 
 export const SERVER_URL =
   process.env.NODE_ENV === 'production'
-    ? window.location.host
+    ? window.location.origin
     : 'http://localhost:9000';
 
 export const WIKI_URL = 'https://wikipedia.org';
